230662578011c113d2b4b62753f2f55b83c5858c
Kalinichenko3 <kalinichenko.m03@gmail.com>
2025-03-06 14:49:35 +0200

    added testsuit for 7.1

--- prj/Калініченко-task_7_1/main.c
+++ prj/Калініченко-task_7_1/main.c
@@ -4,7 +4,6 @@
 
 #define MAX_LEN 1000
 
-// Функция для удаления пробелов и знаков препинания в конце строки
 void trim_trailing_whitespace_and_punctuation(char *str) {
     size_t len = strlen(str);
     while (len > 0 && (str[len - 1] == ' ' || str[len - 1] == '.' || str[len - 1] == ',' || str[len - 1] == '!')) {
@@ -14,19 +13,15 @@ void trim_trailing_whitespace_and_punctuation(char *str) {
 }
 
 int main() {
-    // Устанавливаем локаль для корректного отображения украинского языка
     setlocale(LC_ALL, "uk_UA.UTF-8");
 
     char sentence[MAX_LEN];
 
     printf("Введіть речення: ");
-    // Используем scanf для ввода строки
     scanf("%[^\n]", sentence);
 
-    // Получаем длину строки
     size_t len = strlen(sentence);
 
-    // Проверка корректного завершения предложения
     if (len >= 3 && strcmp(&sentence[len - 3], "...") == 0) {
         printf("Речення має коректне завершення.\n");
     } else if (len >= 1 && sentence[len - 1] == '.') {
@@ -35,12 +30,10 @@ int main() {
         printf("Речення має некоректне завершення.\n");
     }
 
-    // Копируем строку и очищаем её от пунктуации для поиска "масив"
     char sentence_copy[MAX_LEN];
     strcpy(sentence_copy, sentence);
     trim_trailing_whitespace_and_punctuation(sentence_copy);
 
-    // Проверка наличия слова "масив"
     if (strstr(sentence_copy, "масив") != NULL) {
         printf("У реченні є слово 'масив'.\n");
     } else {
